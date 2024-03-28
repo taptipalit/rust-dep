db1b691601a3bfc2728fdec750dbf0d77d223df7
Tapti Palit <tpalit@purdue.edu>
2024-03-28 16:05:40 +0000

    split scripts

--- run.sh
+++ run.sh
@@ -54,6 +54,14 @@ export PATH="/home/tpalit/.cargo/bin:$BIN_PATH:$PATH"
 export LLVM_DIR="$BIN_PATH"
 export LLVM_HOME="$BIN_PATH"
 
+echo "export PATH=\"/home/tpalit/.cargo/bin:$BIN_PATH:$PATH\"" > ~/.bashrc
+echo "export LLVM_DIR=\"$BIN_PATH\"" > ~/.bashrc
+echo "export LLVM_HOME=\"$BIN_PATH\"" > ~/.bashrc
+
+source ~/.bashrc
+
+
+
 if [ ! -d projects ]; then
 	mkdir projects
 fi
@@ -68,39 +76,9 @@ else
 	git pull
 fi
 
-exit 0
 cargo rustc -- --emit=llvm-ir
 
 cd ../..
-rm bitcodes/*
-if [ ! -d bitcodes ]; then
-	mkdir bitcodes
-fi
-find . -name "rg-*.ll" -exec cp {} ./bitcodes \;
 
-cd bitcodes
 
-find . -type f -name "rg-*.ll" -print0 | while IFS= read -r -d '' file; do
-  rustfilt < "$file" > "$file.processed"
-done
 
-cd ..
-
-echo $PWD
-
-# Build the pass
-rm -rf pass-build
-mkdir pass-build
-cd pass-build
-cmake -DLT_LLVM_INSTALL_DIR=$LLVM_DIR $(realpath ../callgraph-pass/Callgraph/)
-make
-
-echo $PWD
-which opt
-cd ../
-# Run the pass
-opt -load-pass-plugin ./pass-build/libCallgraph.so -passes=hello-world -disable-output \
-	bitcodes/rg-*.ll > callgraph.csv
-
-# Convert CSV to JSON
-python3 parseJson.py
